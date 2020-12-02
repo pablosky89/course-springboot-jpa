365ecc0308e5cb32773bea4a751a1626a7334ca3
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2020-12-02 17:46:55 -0300

    Category

---
+++ src/main/java/com/pablosanhueza/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.pablosanhueza.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pablosanhueza.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
