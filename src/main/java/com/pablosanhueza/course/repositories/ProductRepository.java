d74a5bd04244141a2a26e130ebddcefefa2e8245
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2020-12-02 22:45:18 -0300

    Many-to-many association with JoinTable

---
+++ src/main/java/com/pablosanhueza/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.pablosanhueza.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pablosanhueza.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
